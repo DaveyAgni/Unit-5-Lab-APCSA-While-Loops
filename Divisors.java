b63800ab8a4e54df1882cb7fb11a55a9428ae3e5
DaveyAgni <devarchit.agnihotri@icloud.com>
2024-11-01 10:17:54 -0400

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -6,7 +6,20 @@ public class Divisors
 {
 	public static String getDivisors( int number )
 	{
-		String divisors="";
-		return divisors; 
+        int count = number;
+
+        System.out.print("The divisors of " + number + " are: ");
+
+        do{
+            if(number/count != number && number%count == 0)
+            {
+                System.out.print((number/count)+" ");
+            }
+            count--;
+
+        }while(count < number && count > 0);
+
+		System.out.print("");
+		return ""; 
 	}
 }
